8ee1f9f12beef7514444c3ddeb2ca50293f07e1a
FernandoGarcia24 <lfgarcia314@misena.edu.co>
2023-04-07 16:33:22 -0500

    ensayo

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,6 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.js'
 import {BrowserRouter} from 'react-router-dom'
 
-
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
