aa04c191d8ffe339e2496883d39ac3fed34d1fd9
JhonRivera00 <jarivera808@misena.edu.co>
2023-04-12 08:13:56 -0500

    V 1.953 App

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,14 +25,7 @@ const Navbar = () => {
               className="img-logo-mini d-none me-auto"
             />
           </Link>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"
           >
             <span className="navbar-toggler-icon"></span>
           </button>
